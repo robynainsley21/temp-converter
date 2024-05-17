477f35d335bce7149716897bc65a63617d7b78eb
robynainsley21 <robynainsley2i@gmail.com>
2024-05-17 16:16:41 +0200

    Complete error boundary

--- script/script.js
+++ script/script.js
@@ -1,25 +1,40 @@
 const domElements = {
-    celcius: document.getElementById('celcius'),
-    fahrenheit: document.getElementById('fahrenheit'),
-    convertBtn: document.querySelector('[convert-btn]')
+  celcius: document.getElementById("celcius"),
+  fahrenheit: document.getElementById("fahrenheit"),
+  convertBtn: document.querySelector("[convert-btn]"),
+  errorDisplay: document.querySelector("[display]"),
+  clearCelcius: document.querySelector('[clearCelcius]'),
+  clearFah: document.querySelector('[clearFah]'),
+};
+
+domElements.convertBtn.addEventListener("click", () => {
+  let celciusValue = +domElements.celcius.value;
+  let fahrenheitValue = +domElements.fahrenheit.value;
+
+  if (celciusValue && fahrenheitValue) {
+    domElements.errorDisplay.textContent =
+      "Error: Please clear one of the fields.";
+  } else if (celciusValue) {
+    let fahCalc = parseFloat(celciusValue * (9 / 5) + 32).toFixed(2);
+
+    domElements.fahrenheit.value = fahCalc;
+    domElements.errorDisplay.textContent = "";
+  } else if (fahrenheitValue) {
+    let celciusCalc = parseFloat((fahrenheitValue - 32) * (5 / 9)).toFixed(2);
+    domElements.celcius.value = celciusCalc;
+    domElements.errorDisplay.textContent = "";
+  } else console.log("Error");
+});
+
+domElements.clearCelcius.addEventListener('click', () => {
+    domElements.celcius.value = ''
+domElements.errorDisplay.textContent = ''
 }
 
-domElements.convertBtn.addEventListener('click', () => {
-    let celciusValue = domElements.celcius.value
-    let fahrenheitValue = domElements.fahrenheit.value
-    
 
+)
 
-    if(celciusValue ){
-        let celciusCalc = (parseFloat((domElements.fahrenheit - 32) * (5 / 9)))
-        console.log(celciusCalc)
-    } else if(fahrenheitValue ){
-        let fahCalc = (parseFloat((domElements.celcius * (9 / 5)) + 32))
-        
-        console.log(fahCalc)
-
-    } else(
-        console.log('Error')
-    )
-    // console.log(celciusCalc)
+domElements.clearFah.addEventListener('click', () => {
+    domElements.fahrenheit.value = ''
+    domElements.errorDisplay.textContent = ''
 })
